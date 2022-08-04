593f0d8ab00b0b49f2b88925410a43bd7d3c5796
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-04 13:30:59 -0800

    Refactor: Rewrite code into one large function of functions

--- src/listCreator.js
+++ src/listCreator.js
@@ -4,8 +4,30 @@ let schoolList = [];
 let content = document.getElementById('content');
 import {default as createNav} from './nav';
 
-let formatScreen = () =>{
-    content.innerHTML = "";
-    content.appendChild(createNav());
+
+const createList = function (type){
+    void function (){
+        content.innerHTML = "";
+        content.appendChild(createNav());
+    }();
+
+    void function createForm() {
+        let form = document.createElement('form');
+        return form;
+    }();
+
+    let addButton = () => {
+        let button  = document.createElement('button');
+        button.textContent = '+';
+        button.addEventListener('click', () =>{
+            if(createForm().style.display == 'block'){
+                createForm().style.display = 'none'
+            }
+            else{
+                createForm().style.display = 'block';
+            };
+        });
+        content.appendChild(button);
+    }
 };
-export {formatScreen};
+export default createList;
