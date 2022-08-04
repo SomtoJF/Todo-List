9f8f7f94e3b45e4c5568ff6a272d72ea8064b60f
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-04 10:04:23 -0800

    Import todoType.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {default as createNav} from './nav';
+import {default as todoType} from './todoType';
 import './style.css';
 let content = document.getElementById('content');
 
 content.appendChild(createNav());
+content.appendChild(todoType());
