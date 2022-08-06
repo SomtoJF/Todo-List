7f2e35118ecb6108cbe8b74b1d093bd4c3050aa4
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-06 14:48:28 -0800

    Create back Button

--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,6 @@
 import './nav.css';
+import {default as todoType} from './todoType';
+let content = document.getElementById('content');
 
 const createNav = function (){
     let nav = document.createElement('nav');
@@ -6,9 +8,19 @@ const createNav = function (){
     let logo = document.createElement('div');
     logo.setAttribute('id', 'logo');
     logo.innerHTML = '<h1>Oblige</h1>';
-    
-    
+    let menuOptionsDiv = document.createElement('div');
+    let backButton = document.createElement('button');
+    backButton.setAttribute('class', 'link');
+    backButton.textContent = 'back';
+    backButton.addEventListener('click', ()=>{
+        content.innerHTML = '';
+        content.appendChild(createNav());
+        content.appendChild(todoType());
+    });
+
+    menuOptionsDiv.appendChild(backButton);
     nav.appendChild(logo);
+    nav.appendChild(menuOptionsDiv);
     return nav;
 };
 export default createNav;
