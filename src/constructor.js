ce7f36bf3d72f13000bccd4b7a69e76fb3a06d26
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-08-20 14:51:50 -0800

    BugFix: Reformats already formatted date

--- src/constructor.js
+++ src/constructor.js
@@ -1,6 +1,5 @@
 function task (title, description, priority, dateDue){
     const dateCreated = new Date();
-    dateDue = new Date(dateDue);
     return {title, description, priority, dateCreated, dateDue};
 };
 export default task;
